580eb46fd8e89c594eec8678527cbe87d9a0c58e
rfprod <zoidenmacht@zoho.com>
2017-12-20 12:10:00 -0800

    (update) functions: save message to db in case of mail transporter error

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,36 @@
+/**
+ *	Create and Deploy Cloud Functions
+ *	https://firebase.google.com/docs/functions/write-firebase-functions
+ *
+ *	basic usage example
+ *
+ * exports.helloWorld = functions.https.onRequest((request, response) => {
+ *		response.send('Hello from Firebase!');
+ *	});
+ */
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
 const nodemailer = require('nodemailer');
 
-/*
-*	Create and Deploy Cloud Functions
-*	https://firebase.google.com/docs/functions/write-firebase-functions
-*/
-
-/*
-*	basic usage example
-*
-* exports.helloWorld = functions.https.onRequest((request, response) => {
-*		response.send('Hello from Firebase!');
-*	});
-*/
-
-/*
-*	load .env variables
-*/
+/**
+ *	load .env variables
+ */
 require('dotenv').load();
 
-/*
-* nodemailer usage notice:
-* To use Gmail you may need to configure "Allow Less Secure Apps" (https://www.google.com/settings/security/lesssecureapps)
-* in your Gmail account unless you are using 2FA
-* in which case you would have to create an Application Specific password (https://security.google.com/settings/security/apppasswords).
-* You may also need to unlock your account with "Allow access to your Google account" (https://accounts.google.com/DisplayUnlockCaptcha)
-* to use SMTP.
-*/
+/**
+ *	initialize admin SDK to access Firebase Realtime Database
+ */
+admin.initializeApp(functions.config().firebase);
+
+/**
+ * nodemailer usage notice:
+ * To use Gmail you may need to configure "Allow Less Secure Apps" (https://www.google.com/settings/security/lesssecureapps)
+ * in your Gmail account unless you are using 2FA
+ * in which case you would have to create an Application Specific password (https://security.google.com/settings/security/apppasswords).
+ * You may also need to unlock your account with "Allow access to your Google account" (https://accounts.google.com/DisplayUnlockCaptcha)
+ * to use SMTP.
+ */
 const smtpConfig = {
 	host: process.env.MAILER_HOST,
 	port: process.env.MAILER_PORT,
@@ -40,9 +44,10 @@ const smtpConfig = {
 		accessToken: 'empty'
 	}
 };
-/*
-*	reusable transporter object using the default SMTP transport
-*/
+
+/**
+ *	reusable transporter object using the default SMTP transport
+ */
 const mailTransporter = nodemailer.createTransport(smtpConfig);
 mailTransporter.verify((err, success) => {
 	if (err) {
@@ -52,6 +57,27 @@ mailTransporter.verify((err, success) => {
 	}
 });
 
+/**
+ * Fallback function if mail transporter returns an error on sendEmail
+ */
+function saveEmailToDB(name, email, header, message, domain, res) {
+	const entry = {
+		name: name,
+		email: email,
+		header: header,
+		message: message,
+		domain: domain
+	};
+	admin.database().ref('/emails').push(entry).then((snapshot) => {
+		res.status(200).json({success: 'Your message was successfully sent'});
+	}).catch((error) => {
+		res.status(500).send('Error: try again later, please');
+	});
+}
+
+/**
+ * Send email message using nodemailer
+ */
 function sendEmail(name, email, header, message, domain, res) {
 	const mailOptions = {
 		from: '"PORTFOLIO 👥" <' + process.env.MAILER_EMAIL +'>',
@@ -63,7 +89,12 @@ function sendEmail(name, email, header, message, domain, res) {
 	mailTransporter.sendMail(mailOptions, (err, info) => {
 		if (err) {
 			// console.log('Mail transporter error:', err);
-			res.status(500).send('Mail transporter error');
+			/*
+			*	do not report error to user yet
+			*	try recording message to DB first
+			*/
+			// res.status(500).send('Mail transporter error');
+			saveEmailToDB(name, email, header, message, domain, res);
 		} else {
 			// console.log('Message sent: ' + info.response);
 			res.status(200).json({success: 'Your message was successfully sent'});
@@ -71,6 +102,9 @@ function sendEmail(name, email, header, message, domain, res) {
 	});
 }
 
+/**
+ * actual send email message cloud function
+ */
 exports.sendEmail = functions.https.onRequest((req, res) => {
 	if (req.method !== 'POST') {
 		res.status(403).json({error: 'Forbidden method'});
